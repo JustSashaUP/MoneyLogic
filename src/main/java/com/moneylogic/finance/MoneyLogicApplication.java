400330aea2bd58c240df5e208d894b67a1200ddc
Oleksandr Savchenko <alexandr.savchenko04@gmail.com>
2025-01-18 15:09:10 +0200

    init

---
+++ src/main/java/com/moneylogic/finance/MoneyLogicApplication.java
@@ -0,0 +1,13 @@
+package com.moneylogic.finance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneyLogicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoneyLogicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/moneylogic/finance/MoneyLogicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moneylogic.finance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneyLogicApplicationTests {
+
+    //@Test
+    void contextLoads() {
+    }
+
+}
