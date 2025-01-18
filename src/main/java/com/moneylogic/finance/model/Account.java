d24074dd1120e850bc77951ba2b2685122f37bf2
Oleksandr Savchenko <alexandr.savchenko04@gmail.com>
2025-01-18 20:08:41 +0200

    add POJO class Account

---
+++ src/main/java/com/moneylogic/finance/model/Account.java
@@ -0,0 +1,40 @@
+package com.moneylogic.finance.model;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.Objects;
+
+@Setter
+@Getter
+@ToString
+public class Account {
+    private long id;
+    private User user;
+    private String name;
+    BigDecimal balance;
+    /**
+     * Date when the user created the current account
+     * */
+    LocalDate accountCreatedDate;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Account account = (Account) o;
+        return id == account.id &&
+                Objects.equals(user, account.user) &&
+                Objects.equals(name, account.name) &&
+                Objects.equals(balance, account.balance) &&
+                Objects.equals(accountCreatedDate, account.accountCreatedDate);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, user, name, balance, accountCreatedDate);
+    }
+}
