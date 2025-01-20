c1f91e8e8ff1b9c00750a164fcb906f0826dca0f
Oleksandr Savchenko <alexandr.savchenko04@gmail.com>
2025-01-20 20:16:01 +0200

    add Transaction and Catalogs fields add static factory methods

--- src/main/java/com/moneylogic/finance/model/Account.java
+++ src/main/java/com/moneylogic/finance/model/Account.java
@@ -1,5 +1,6 @@
 package com.moneylogic.finance.model;
 
+import com.moneylogic.finance.util.CommonUtils;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -11,7 +12,6 @@ import java.util.Objects;
 
 @Setter
 @Getter
-@ToString
 public class Account {
     private long id;
     private User user;
@@ -25,9 +25,36 @@ public class Account {
      * Transactions related to the user's account
      * */
     private List<Transaction> transactions;
-    //TODO is Account have diff catalogs or User?
+    /**
+     * Catalog templates related to the user's account
+     * */
+    private List<Catalog> catalogs;
 
-    public Account() {}
+    private Account() {}
+
+    public static Account createAccount(User user, String name, BigDecimal balance,
+                                        List<Transaction> transactions, List<Catalog> catalogs, String accountCreatedDate) {
+        Account account = new Account();
+        account.user = user;
+        account.name = name;
+        account.balance = balance;
+        account.transactions = transactions;
+        account.catalogs = catalogs;
+        account.accountCreatedDate = CommonUtils.parseToLocalDate(accountCreatedDate);
+        return account;
+    }
+
+    public static Account createAccountWithCurrentDate(User user, String name, BigDecimal balance,
+                                                       List<Transaction> transactions, List<Catalog> catalogs) {
+        Account account = new Account();
+        account.user = user;
+        account.name = name;
+        account.balance = balance;
+        account.transactions = transactions;
+        account.catalogs = catalogs;
+        account.accountCreatedDate = LocalDate.now();
+        return account;
+    }
 
     @Override
     public boolean equals(Object o) {
@@ -44,5 +71,18 @@ public class Account {
     @Override
     public int hashCode() {
         return Objects.hash(id, user, name, balance, accountCreatedDate);
+    }
+
+    @Override
+    public String toString() {
+        return "Account{" +
+                "id=" + id +
+                ", user=" + user +
+                ", name='" + name + '\'' +
+                ", balance=" + balance +
+                ", accountCreatedDate=" + accountCreatedDate +
+                ", transactions=" + transactions +
+                ", catalogs=" + catalogs +
+                '}';
     }
 }
