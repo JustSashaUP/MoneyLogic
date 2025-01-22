7d086c0da9760c07fd969b6142ff25cd707e08d1
Oleksandr Savchenko <alexandr.savchenko04@gmail.com>
2025-01-22 19:32:53 +0200

    add enum constants and methods && rename enum⚙️

---
+++ src/main/java/com/moneylogic/finance/model/TransactionType.java
@@ -0,0 +1,29 @@
+package com.moneylogic.finance.model;
+
+import java.math.BigDecimal;
+
+public enum TransactionType {
+    INCOME("income") {
+        public BigDecimal apply(BigDecimal amount, BigDecimal total) {
+            return total.add(amount);
+        }
+    },
+    EXPENSE("expense") {
+        public BigDecimal apply(BigDecimal amount, BigDecimal total) {
+            return total.subtract(amount); //TODO What if total < amount (total == 0)
+        }
+    };
+
+    private final String type;
+
+    TransactionType(String type) {
+        this.type = type;
+    }
+
+    @Override
+    public String toString() {
+        return "Transaction Type = " + type;
+    }
+
+    public abstract BigDecimal apply(BigDecimal amount, BigDecimal total);
+}
