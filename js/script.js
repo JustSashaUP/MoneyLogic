028f437da10285776a93b69ec048487080daa36e
Denis Pudlo <crisehop@gmail.com>
2025-01-22 17:48:05 +0100

    adapted index.html, added password hiding, made a theme switch, but not implemented yet

--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
+//===============MODALS=====================//
+//===============MODALS=====================//
+
 const registrationLinkElements = document.querySelectorAll('[data-js-registration-link]')
 const registrationFormElement = document.querySelector('[data-js-registration-form]')
-
-console.log(registrationLinkElements)
 
 registrationLinkElements.forEach((link) => {
   link.addEventListener('click', () => {
@@ -15,3 +16,24 @@ const loginFormElement = document.querySelector('[data-js-login-form]')
 loginLinkElement.addEventListener('click', () => {
   loginFormElement.showModal()
 })
+
+//===============HIDING PASSWORD=====================//
+//===============HIDING PASSWORD=====================//
+
+const passwordHideButtonElements = document.querySelectorAll('[data-js-password-hide-button]');
+const passwordInputElements = document.querySelectorAll('[data-js-password-input]');
+const fieldPasswordWrapperElements = document.querySelectorAll('[data-js-field-password-wrapper]');
+
+for (let i = 0; i < passwordHideButtonElements.length; i++) {
+  passwordHideButtonElements[i].addEventListener('click', () => {
+    const isPassword = passwordInputElements[i].type === 'password';
+
+    if (isPassword) {
+      passwordInputElements[i].type = 'text';
+      fieldPasswordWrapperElements[i].style.setProperty('--line-opacity', '0');
+    } else {
+      passwordInputElements[i].type = 'password';
+      fieldPasswordWrapperElements[i].style.setProperty('--line-opacity', '1');
+    }
+  });
+}
