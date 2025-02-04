d1edb58584176ab311ead71fa79f8724a4d9ab9f
Oleksandr Savchenko <alexandr.savchenko04@gmail.com>
2025-02-04 10:38:17 +0200

    added @Transaction annotation

--- src/main/java/com/moneylogic/finance/service/CustomUserDetailsService.java
+++ src/main/java/com/moneylogic/finance/service/CustomUserDetailsService.java
@@ -1,9 +1,10 @@
 package com.moneylogic.finance.service;
 
 import com.moneylogic.finance.logging.LoggerSingleton;
-import com.moneylogic.finance.model.MyUserDetails;
+import com.moneylogic.finance.model.CustomUserDetails;
 import com.moneylogic.finance.model.User;
 import com.moneylogic.finance.repository.User.UserRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -13,16 +14,17 @@ import org.springframework.stereotype.Service;
 import java.util.Optional;
 
 @Service
-public class MyUserDetailsService implements UserDetailsService {
+public class CustomUserDetailsService implements UserDetailsService {
     @Autowired
     private UserRepository repository;
 
     @Override
+    @Transactional
     public UserDetails loadUserByUsername(String login) throws UsernameNotFoundException {
         Optional<User> user = repository.findByEmailOrUsername(login,login);
-        LoggerSingleton.info(MyUserDetails.class, "Loaded user: " + user);
+        LoggerSingleton.info(CustomUserDetails.class, "Loaded user: " + user);
 
-        return user.map(MyUserDetails::new)
+        return user.map(CustomUserDetails::new)
                 .orElseThrow(() -> new UsernameNotFoundException("User not found with login: " + login));
     }
 }
